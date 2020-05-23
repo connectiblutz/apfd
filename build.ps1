c485aba3f213c4e0afdba166eb9a9e5748618813
Brian Lutz <blutz@connectify.me>
2020-05-22 20:53:46 -0400

    build system

---
+++ build.ps1
@@ -0,0 +1,84 @@
+param (
+  [switch]$all,
+  [switch]$debug,
+  [switch]$release,
+  [switch]$x64,
+  [switch]$x86,
+  [switch]$clean
+)
+
+if (!$x64 -and !$x86) {
+  $x64=$true
+}
+if (!$release -and !$debug) {
+  $release=$true
+}
+if ($all) {
+  $x64=$true
+  $x86=$true
+  $release=$true
+  $debug=$true
+}
+
+Import-Module -Force $PSScriptRoot\common.psm1
+
+function clean {
+  Param($arch, $config)
+
+  echo "Cleaning up $arch $config ..."
+
+  removeFolder _builds/$arch/$config
+}
+function setup {
+  Param($arch, $config)
+
+  echo "Setting up $arch $config ..."
+
+  if (-Not (folderExists _builds/$arch/$config)) {
+    createFolder _builds/$arch/$config
+  }
+  cd _builds/$arch/$config
+  $env:VSCMD_ARG_TGT_ARCH=$arch
+  cmake ..\..\.. -GNinja -DCMAKE_BUILD_TYPE=config -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+  CheckLastExitCode 0
+  cd ..\..\..
+}
+function build {
+  Param($arch, $config)
+
+  echo "Building $arch $config ..."
+
+  $env:VSCMD_ARG_TGT_ARCH=$arch
+  ninja -C _builds/$arch/$config
+  CheckLastExitCode 0
+}
+
+function fullArchSteps {
+  Param($arch, $config)
+  if ($clean) { clean $arch $config }
+  setup $arch $config
+  build $arch $config
+}
+
+PUSHD
+
+try {
+
+  if ($x64) {
+    if ($release) {
+      fullArchSteps "x64" "release"
+    }
+    if ($debug) {
+      fullArchSteps "x64" "debug"
+    }
+  }
+  if ($x86) {
+    if ($release) {
+      fullArchSteps "x86" "release"
+    }
+    if ($debug) {
+      fullArchSteps "x86" "debug"
+    }
+  }
+
+} finally { POPD }
