bbd6b493b437f5fae09bccd9bb464c59aab4d315
Brian Lutz <brian@blutz.net>
2020-05-30 15:54:26 -0400

    log and file util; fix issue with message posted in constructor; read in config file

--- src/apfdworker.cc
+++ src/apfdworker.cc
@@ -1,5 +1,8 @@
 #include "apfdworker.h"
 #include <iostream>
+#include "fileutil.h"
+#include "cJSON.h"
+#include "logutil.h"
 
 namespace apfd {
 
@@ -11,6 +14,12 @@ ApfdWorker::ApfdWorker() {
 
 void ApfdWorker::OnMessage(common::MessageThread::Message message) {
     std::cout << "received message "<<message.code() << std::endl;
+    if (message.code()==ApfdWorker::MSG_READCONFIG) {
+      std::string configStr = common::FileUtil::FileToString("apfd.json");
+      cJSON *config = cJSON_Parse(configStr.c_str());
+      LOG(std::cout,cJSON_Print(config));
+      cJSON_free(config);
+    }
 }
 
 }
