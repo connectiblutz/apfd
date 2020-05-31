7815c7e2c488723d6dae4b6ed289d297deafb6dc
Brian Lutz <brian@blutz.net>
2020-05-31 17:57:26 -0400

    singleton class

--- src/run.cc
+++ src/run.cc
@@ -7,14 +7,14 @@ namespace apfd {
 
 int runAsApp() {
   auto runnerThread = std::make_shared<ApfdWorker>();
-  auto ch = common::ConsoleHandler::make_shared(std::dynamic_pointer_cast<common::MessageThread>(runnerThread));
+  auto ch = common::Singleton::weak<common::ConsoleHandler>(std::dynamic_pointer_cast<common::MessageThread>(runnerThread));
   runnerThread->join();
   return 0;
 }
 
 int runAsDaemon() {
   auto runnerThread = std::make_shared<ApfdWorker>();
-  auto ws = common::WindowsService::make_shared(std::string("apfd"),std::dynamic_pointer_cast<common::MessageThread>(runnerThread));
+  auto ws = common::Singleton::weak<common::WindowsService>(std::string("apfd"),std::dynamic_pointer_cast<common::MessageThread>(runnerThread));
   return 0;
 }
 
