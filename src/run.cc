0e09b4dff4967aaa00f1c93a9cbc1ca5d1837c72
Brian Lutz <brian@blutz.net>
2020-05-31 13:14:16 -0400

    refactor

--- src/run.cc
+++ src/run.cc
@@ -6,14 +6,15 @@
 namespace apfd {
 
 int runAsApp() {
-  auto runnerThread = new ApfdWorker();
-  auto ch = common::ConsoleHandler::make_shared(std::shared_ptr<common::MessageThread>(runnerThread));
+  auto runnerThread = std::make_shared<ApfdWorker>();
+  auto ch = common::ConsoleHandler::make_shared(std::dynamic_pointer_cast<common::MessageThread>(runnerThread));
   runnerThread->join();
   return 0;
 }
 
 int runAsDaemon() {
-  auto ws = common::WindowsService::make_shared(std::string("apfd"),std::shared_ptr<common::MessageThread>(new ApfdWorker()));
+  auto runnerThread = std::make_shared<ApfdWorker>();
+  auto ws = common::WindowsService::make_shared(std::string("apfd"),std::dynamic_pointer_cast<common::MessageThread>(runnerThread));
   return 0;
 }
 
