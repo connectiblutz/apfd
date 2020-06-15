5207cb207508c4b44f2a44526a50d11bf2a7d249
Brian Lutz <blutz@connectify.me>
2020-06-15 19:35:29 -0400

    implement running as daemon and consolehandler

--- apfd/src/run.cc
+++ apfd/src/run.cc
@@ -6,6 +6,10 @@
 #include <filesystem>
 #include <string>
 
+#ifndef _WIN32
+#include <unistd.h>
+#endif
+
 namespace apfd {
 
 int runAsApp() {
@@ -17,14 +21,18 @@ int runAsApp() {
 }
 
 int runAsDaemon() {
-#ifdef _WIN32
   auto logPath = std::filesystem::temp_directory_path();
   logPath/="apfd.log";
   common::Singleton::Strong::create<common::LogUtil>(logPath);
   auto runnerThread = std::make_shared<ApfdWorker>();
   auto mt = std::dynamic_pointer_cast<common::MessageThread>(runnerThread);
+#ifdef _WIN32
   auto ws = common::Singleton::Weak::create<common::WindowsService>(std::string("apfd"),mt);
   ws->start();
+#else
+  if (fork()==0) {
+    mt->join();
+  }
 #endif
   return 0;
 }
